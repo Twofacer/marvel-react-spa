e8a458d0c2bad621ca85f7fd0bc961bd29b4c38f
VioZero <viocar112@gmail.com>
2021-12-06 12:30:13 +0300

    add react lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+import Spinner from '../spinner/Spinner';
+import {lazy, Suspense} from 'react';
 import AppHeader from "../appHeader/AppHeader";
-
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const App = () => {
     
     return (
@@ -10,6 +13,7 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
+                <Suspense fallback={<Spinner/>}>
                 <Routes>
                                 <Route exact path="/comics" element={<ComicsPage/>}>
                                 </Route>
@@ -20,7 +24,9 @@ const App = () => {
                                 <Route path="/" element={<MainPage/>}></Route>
                                 <Route path="*"element={<Page404/>}></Route>
                             </Routes>
+                            </Suspense>
                 </main>
+                
             </div>
         </Router>
     )
